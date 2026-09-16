857f12843d66b7fda6b804ef7c0ea4ae19e4a54c
Jeremy Callinan <jeremy.callinan@gmail.com>
2026-09-15 20:13:57 -0400

    On Page 781

---
+++ MD/Assets/SlingShot.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class SlingShot : MonoBehaviour
+{
+    public GameObject launchPoint;
+    public GameObject projectilePrefab;
+    public float velocityMult = 10f;
+    public Vector3 launchPos;
+    public GameObject projectile;
+    public bool aimingMode;
+
+
+    private void Awake()
+    {
+        Transform launchPointTrans = transform.Find("LaunchPoint");
+        launchPoint = launchPointTrans.gameObject;
+        launchPoint.SetActive(false);
+        launchPos = launchPoint.transform.position;
+    }
+    void OnMouseEnter()
+    {
+        //print("SlingShot: OnMouseEnter");
+        launchPoint.SetActive(true);
+    }
+    void OnMouseExit()
+    {
+        //print("SlingShot: OnMouseExit");
+        launchPoint.SetActive(false);
+    }
+
+    void OnMouseDown()
+    {
+        aimingMode = true;
+        //instantiate a projectile
+        projectile = Instantiate(projectilePrefab) as GameObject;
+        projectile.transform.position = launchPos;
+        projectile.GetComponent<Rigidbody>().isKinematic = true;
+
+    }
+
+    private void Update()
+    {
+        if (!aimingMode) return;
+
+        //get the current mouse position in 2D screen coordinates
+        Vector3 mousePos2D = Input.mousePosition;
+        mousePos2D.z = -Camera.main.transform.position.z;
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+
+        //find the delta from the launchPos to the mousePos3D
+        Vector3 mouseDelta = mousePos3D - launchPos;
+        float maxMagnitude = this.GetComponent<SphereCollider>().radius;
+        if (mouseDelta.magnitude > maxMagnitude)
+        {
+            mouseDelta.Normalize();
+            mouseDelta *= maxMagnitude;
+        }
+        //move the projectile to this new position
+        Vector3 projPos = launchPos + mouseDelta;
+        projectile.transform.position = projPos;
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            //the mouse has been released
+            aimingMode = false;
+            Rigidbody projRB = projectile.GetComponent<Rigidbody>();
+            projRB.isKinematic = false;
+            projRB.collisionDetectionMode= CollisionDetectionMode.Continuous;
+            projRB.velocity = -mouseDelta * velocityMult;
+            projectile = null;
+        }
+    }
+}
